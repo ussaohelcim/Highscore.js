0c68320088ae5258d05008da9ccddd63f4d56a32
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-05-26 15:49:31 -0300

    added count games method

--- highscore.ts
+++ highscore.ts
@@ -5,9 +5,8 @@ interface IScore{
 }
 
 enum ResponseTypes{
-	AddedScore = 1,
-	ScoreNotAdded = 2,
-	NewHighscore = 6
+	ScoreAdded = 1,
+	ScoreNotAdded = 2
 }
 
 class HighscoreSDK{
@@ -17,24 +16,32 @@ class HighscoreSDK{
 	constructor(apiURL:string){
 		this.apiURL = apiURL
 	}
+
+	/**
+	 * Returns all scores from the database between 1 and 10 (inclusive).
+	 * @returns {IScore[]}
+	 */
 	async getTop10():Promise<IScore[]>{
-		let scoreBoard:IScore[] = await fetch(`${this.apiURL}games/top`)
+		let scoreBoard:IScore[] = await fetch(`${this.apiURL}api/top`)
 		.then((res)=>{
-			console.log(`${this.apiURL}games/top`)
 			return res.json()
-		}).then((body)=>{
-			return body
 		})
 
 		return scoreBoard
 	}
 
+	/**
+	 * Adds a new score to the database and returns a ResponseTypes.
+	 * @param playerName 
+	 * @param score 
+	 * @returns {number} 0: error,1: accepted, 2: not accepted
+	 */
 	async addScore(playerName:string,score:number):Promise<number>{
 		let obj = {
 			name: playerName,
 			score: score
 		}
-		return await fetch(`${this.apiURL}games/add`,{
+		return await fetch(`${this.apiURL}api/add`,{
 			method:'Post',
 			headers:{
 				'Content-Type': 'application/json;charset=utf-8'
@@ -43,24 +50,56 @@ class HighscoreSDK{
 		}).then((res)=>{
 			if(res.status === 202)
 			{
-
+				return ResponseTypes.ScoreAdded
 			}
 			else if(res.status === 403)
 			{		
-
+				return ResponseTypes.ScoreNotAdded
 			}
-			console.log(res.statusText)
+			
 			return 0
 		})
 	}
+	/**
+	 * Returns all scores from the database between {start} and {end} (inclusive).
+	 * @param start 
+	 * @param end 
+	 * @returns {IScore[]}
+	 */
 	async getBetween(start:number,end:number):Promise<IScore[]>{
-		return await fetch(`${this.apiURL}games/get/${start}/${end}`)
+		return await fetch(`${this.apiURL}api/get/${start}/${end}`)
 		.then((res)=>{
 			return res.json()
 		})
 	}
+
+	/**
+	 * Returns all scores from the database.
+	 * @returns {IScore[]}
+	 */
+	async getAllScores():Promise<IScore[]>{
+		return await fetch(`${this.apiURL}api/all`).then((res)=>{
+			return res.json()
+		})
+	}
+
+	/**
+	 * Returns the amount of games on database. 
+	 * @returns {number}
+	 */
+	async getLeaderBoardSize(){
+		return await fetch(`${this.apiURL}api/count`).then((res)=>{
+			return res
+		})
+	}
+
+	/**
+	 * 
+	 * @param playername 
+	 * @returns {IScore} score
+	 */
 	async getPlayerScore(playername:string){
-		return await fetch(`${this.apiURL}games/get/${playername}`)
+		return await fetch(`${this.apiURL}api/get/${playername}`)
 		.then((res)=>{
 			return res.json();
 		})
